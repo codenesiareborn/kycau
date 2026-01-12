b2751f6d70bed38839539ee3348a896e7e63c775
Almafazi <almafazi@icloud.com>
2026-01-12 18:19:36 +0700

    fix zoom

--- resources/views/filament/pages/dashboard-footer.blade.php
+++ resources/views/filament/pages/dashboard-footer.blade.php
@@ -148,14 +148,13 @@
                     </div>
                 `);
             
-            // Zoom and center on click
+            // Zoom and center on click, then open popup after animation
             marker.on('click', (e) => {
-                customerMapInstance.setView(e.latlng, 18);
-            });
-
-            // Zoom and center on click
-            marker.on('click', (e) => {
-                customerMapInstance.setView(e.latlng, 18);
+                const latlng = e.latlng;
+                customerMapInstance.flyTo(latlng, 16, { duration: 0.5 });
+                setTimeout(() => {
+                    marker.openPopup();
+                }, 500);
             });
 
             // Add marker to cluster instead of directly to map
